878a914a25c0047ba12e7701a6c5d3cb60ebca33
baileyrachael25 <baileyrachael25@gmail.com>
2022-11-30 21:42:45 -0800

    initial commit

---
+++ Develop/index.js
@@ -0,0 +1,11 @@
+//import libraries and classes
+
+//start app
+
+//creates new employee object
+
+//this prompt asks the user what they would like to do (ie add employees or generate apps)
+
+//calls to template.js to generate page
+
+//initializes app
